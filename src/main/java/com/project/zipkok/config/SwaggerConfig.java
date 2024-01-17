09379087fec56bedf9c87dcb4bc2d6eca652d6b9
encoreJeong <zzaibean@gmail.com>
2024-01-17 18:16:26 +0900

    Fix: Fixed an issue where curl requests were sent to localhost in Swagger

--- src/main/java/com/project/zipkok/config/SwaggerConfig.java
+++ src/main/java/com/project/zipkok/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 
 @Configuration
@@ -25,7 +28,9 @@ public class SwaggerConfig {
                 .components(new Components())
                 .info(apiInfo())
                 .addSecurityItem(securityRequirement)
-                .components(components);
+                .components(components)
+                .servers(Arrays.asList(new Server().url("https://zipkok.store"),
+                        new Server().url("http://localhost:9000")));
     }
     private Info apiInfo() {
         return new Info()
