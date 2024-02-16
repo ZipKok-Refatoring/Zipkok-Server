edaeea3687bbb0415d1bb2c116749ed0e78c1834
KMH <seaweed8759@naver.com>
2024-02-16 22:56:59 +0900

    주석처리

--- src/main/java/com/project/zipkok/controller/CrawlingController.java
+++ src/main/java/com/project/zipkok/controller/CrawlingController.java
@@ -1,16 +1,16 @@
-package com.project.zipkok.controller;
-
-import com.project.zipkok.util.HtmlCrawling;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
+//package com.project.zipkok.controller;
+//
+//import com.project.zipkok.util.HtmlCrawling;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.IOException;
 
 //@Slf4j
 //@RestController
