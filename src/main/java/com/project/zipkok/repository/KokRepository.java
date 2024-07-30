e735947b5bb34dc85625c00cbe154569ec1df2d4
encoreJeong <zzaibean@gmail.com>
2024-07-30 23:19:21 +0900

    remove: 사용하지 않는 import 삭제

--- src/main/java/com/project/zipkok/repository/KokRepository.java
+++ src/main/java/com/project/zipkok/repository/KokRepository.java
@@ -2,8 +2,6 @@ package com.project.zipkok.repository;
 
 import com.project.zipkok.dto.GetKokWithZimStatus;
 import com.project.zipkok.model.Kok;
-import com.project.zipkok.model.RealEstate;
-import com.project.zipkok.model.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 
