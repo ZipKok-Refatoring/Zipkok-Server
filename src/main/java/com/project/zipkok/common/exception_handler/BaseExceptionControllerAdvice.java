38e5301d8f8bc4a4d4bdc481fefc9c21064a1ee7
encoreJeong <zzaibean@gmail.com>
2024-08-06 12:02:56 +0900

    refactor: dto의 validation error를 exception handler에서 처리하도록 수정

--- src/main/java/com/project/zipkok/common/exception_handler/BaseExceptionControllerAdvice.java
+++ src/main/java/com/project/zipkok/common/exception_handler/BaseExceptionControllerAdvice.java
@@ -6,8 +6,12 @@ import com.project.zipkok.common.exception.InternalServerErrorException;
 import com.project.zipkok.common.response.BaseExceptionResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,15 +29,6 @@ public class BaseExceptionControllerAdvice {
         log.error("[handle_BadRequest]", e);
         return new BaseExceptionResponse(URL_NOT_FOUND);
     }
-
-    // 위와 동일 (return ResponseEntity<>)
-    /*
-    @ExceptionHandler({BadRequestException.class, NoHandlerFoundException.class, TypeMismatchException.class, HttpRequestMethodNotSupportedException.class})
-    public ResponseEntity<BaseErrorResponse> handle_BadRequest(BadRequestException e) {
-        log.error("[handle_BadRequest]", e);
-        return ResponseEntity.badRequest().body(new BaseErrorResponse(e.getExceptionStatus()));
-    }
-     */
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
@@ -61,6 +56,28 @@ public class BaseExceptionControllerAdvice {
     public BaseExceptionResponse handle_RuntimeException(Exception e) {
         log.error("[handle_RuntimeException]", e);
         return new BaseExceptionResponse(SERVER_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseExceptionResponse handle_MethodArgumentNotValid(MethodArgumentNotValidException e) {
+        log.error("[handle_MethodArgumentNotValid]", e);
+
+        return new BaseExceptionResponse(BAD_REQUEST, getErrorMessage(e.getBindingResult()));
+    }
+
+    private String getErrorMessage(BindingResult bindingResult) {
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+        return builder.toString();
     }
 
 }
--- src/main/java/com/project/zipkok/controller/UserController.java
+++ src/main/java/com/project/zipkok/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.project.zipkok.controller;
 
 import com.project.zipkok.common.argument_resolver.PreAuthorize;
-import com.project.zipkok.common.exception.user.OnBoardingBadRequestException;
-import com.project.zipkok.common.exception.user.UserBadRequestException;
 import com.project.zipkok.common.response.BaseResponse;
 import com.project.zipkok.dto.*;
 import com.project.zipkok.service.UserService;
@@ -10,15 +8,13 @@ import com.project.zipkok.util.jwt.AuthTokens;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import static com.project.zipkok.common.response.status.BaseExceptionResponseStatus.*;
-import static com.project.zipkok.util.BindingResultUtils.getErrorMessages;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
@@ -32,64 +28,16 @@ public class UserController {
 
     @Operation(summary = "회원가입 API", description = "온보딩 전 회원정보를 입력하는 API입니다.")
     @PostMapping("")
-    public BaseResponse<AuthTokens> signUp(@Validated @RequestBody PostSignUpRequest postSignUpRequest, BindingResult bindingResult) {
+    public BaseResponse<AuthTokens> signUp(@Valid @RequestBody PostSignUpRequest postSignUpRequest) {
         log.info("{UserController.signUp}");
-
-        if(bindingResult.hasFieldErrors("nickname")){
-            throw new UserBadRequestException(INVALID_NICKNAME_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("gender")){
-            throw new UserBadRequestException(INVALID_GENDER_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("birthday")){
-            throw new UserBadRequestException(INVALID_BIRTHDAY_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("email")){
-            throw new UserBadRequestException(INVALID_EMAIL_REQUEST);
-        }
-        if(bindingResult.hasErrors()){
-            throw new UserBadRequestException(BAD_REQUEST);
-        }
 
         return new BaseResponse<>(REGISTRATION_SUCCESS, this.userService.signUp(postSignUpRequest));
     }
 
     @Operation(summary = "온보딩정보 입력 API", description = "회원가입 후, 온보딩 정보를 입력하는 API입니다.")
     @PatchMapping("")
-    public BaseResponse<Object> onBoarding(@Parameter(hidden=true) @PreAuthorize long userId, @Validated @RequestBody PatchOnBoardingRequest patchOnBoardingRequest, BindingResult bindingResult){
+    public BaseResponse<Object> onBoarding(@Parameter(hidden=true) @PreAuthorize long userId, @Valid @RequestBody PatchOnBoardingRequest patchOnBoardingRequest){
         log.info("{UserController.onBoarding}");
-        System.out.println(patchOnBoardingRequest.toString());
-        if(bindingResult.hasFieldErrors("address")){
-            throw new OnBoardingBadRequestException(ADDRESS_OVER_LENGTH);
-        }
-        if(bindingResult.hasFieldErrors("latitude") || bindingResult.hasFieldErrors("longitude")){
-            throw new OnBoardingBadRequestException(INVALID_LAT_LNG);
-        }
-        if(bindingResult.hasFieldErrors("mpriceMin") ||
-            bindingResult.hasFieldErrors("mdepositMin") ||
-            bindingResult.hasFieldErrors("ydepositMin") ||
-            bindingResult.hasFieldErrors("purchaseMin")){
-            throw  new OnBoardingBadRequestException(INVALID_MIN_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("mpriceMax") ||
-            bindingResult.hasFieldErrors("mdepositMax") ||
-            bindingResult.hasFieldErrors("ydepositMax") ||
-            bindingResult.hasFieldErrors("purchaseMax")){
-            throw new OnBoardingBadRequestException(INVALID_MAX_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("realEstateType")){
-            throw new OnBoardingBadRequestException(INVALID_INTEREST_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("transactionType")){
-            throw new OnBoardingBadRequestException(INVALID_TRANSACTION_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("isSmallerthanMax")){
-            throw new OnBoardingBadRequestException(MIN_IS_BIGGER_THAN_MAX);
-        }
-
-        if(bindingResult.hasErrors()){
-            throw new OnBoardingBadRequestException(BAD_REQUEST, getErrorMessages(bindingResult));
-        }
 
         return new BaseResponse(MEMBER_INFO_UPDATE_SUCCESS, this.userService.setOnBoarding(patchOnBoardingRequest, userId));
 
@@ -113,45 +61,8 @@ public class UserController {
 
     @Operation(summary = "프로필 수정하기 API", description = "프로필 수정하기 기능을 담당하는 API입니다.")
     @PutMapping(value = "", consumes = {APPLICATION_JSON_VALUE, MULTIPART_FORM_DATA_VALUE})
-    public BaseResponse<Object> updateMyInfo(@Parameter(hidden = true) @PreAuthorize long userId, @RequestPart(value = "data", required = false) PutUpdateMyInfoRequest putUpdateMyInfoRequest, @RequestPart(value = "file", required = false) MultipartFile file, BindingResult bindingResult){
+    public BaseResponse<Object> updateMyInfo(@Parameter(hidden = true) @PreAuthorize long userId, @RequestPart(value = "data", required = false) PutUpdateMyInfoRequest putUpdateMyInfoRequest, @RequestPart(value = "file", required = false) MultipartFile file){
         log.info("{UserController.updateMyInfo}");
-
-        if(bindingResult.hasFieldErrors("nickname")){
-            throw new UserBadRequestException(INVALID_NICKNAME_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("gender")){
-            throw new UserBadRequestException(INVALID_GENDER_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("birthday")){
-            throw new UserBadRequestException(INVALID_BIRTHDAY_FORMAT);
-        }
-        if(bindingResult.hasFieldErrors("realEstateType")){
-            throw new UserBadRequestException(INVALID_INTEREST_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("address")){
-            throw new UserBadRequestException(ADDRESS_OVER_LENGTH);
-        }
-        if(bindingResult.hasFieldErrors("latitude") || bindingResult.hasFieldErrors("longitude")){
-            throw new UserBadRequestException(INVALID_LAT_LNG);
-        }
-        if(bindingResult.hasFieldErrors("transactionType")){
-            throw new UserBadRequestException(INVALID_TRANSACTION_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("mpriceMin") ||
-                bindingResult.hasFieldErrors("mdepositMin") ||
-                bindingResult.hasFieldErrors("ydepositMin") ||
-                bindingResult.hasFieldErrors("purchaseMin")){
-            throw  new UserBadRequestException(INVALID_MIN_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("mpriceMax") ||
-                bindingResult.hasFieldErrors("mdepositMax") ||
-                bindingResult.hasFieldErrors("ydepositMax") ||
-                bindingResult.hasFieldErrors("purchaseMax")){
-            throw new UserBadRequestException(INVALID_MAX_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("isSmallerthanMax")){
-            throw new UserBadRequestException(MIN_IS_BIGGER_THAN_MAX);
-        }
 
         return new BaseResponse<>(MEMBER_INFO_UPDATE_SUCCESS, this.userService.updateMyInfo(userId, file, putUpdateMyInfoRequest));
     }
@@ -166,16 +77,11 @@ public class UserController {
 
     @Operation(summary = "마이페이지 리스트 항목 수정 API", description = "콕리스트 항목 설정 정보를 수정")
     @PutMapping("/kokOption")
-    public BaseResponse<Object> updateKokOption(@Parameter(hidden=true) @PreAuthorize long userId, @Validated @RequestBody PostUpdateKokOptionRequest postUpdateKokOptionRequest, BindingResult bindingResult){
+    public BaseResponse<Object> updateKokOption(@Parameter(hidden=true) @PreAuthorize long userId, @Valid @RequestBody PostUpdateKokOptionRequest postUpdateKokOptionRequest){
         log.info("{UserController.updateKokOption}");
-
-        if(bindingResult.hasErrors()){
-            throw new UserBadRequestException(MEMBER_LIST_ITEM_UPDATE_FAILURE);
-        }
 
         return new BaseResponse<>(MEMBER_LIST_ITEM_UPDATE_SUCCESS, this.userService.updateKokOption(userId, postUpdateKokOptionRequest));
     }
-
 
     @Operation(summary = "로그아웃 API", description = "회원 로그아웃을 위한 api입니다.")
     @GetMapping("/logout")
@@ -195,29 +101,8 @@ public class UserController {
 
     @Operation(summary = "필터 API", description = "필터정보 수정을 위한 api입니다.")
     @PatchMapping("/filter")
-    public BaseResponse<Object> updateFilter(@Parameter(hidden=true) @PreAuthorize long userId, @Validated @RequestBody PatchUpdateFilterRequest patchUpdateFilterRequest, BindingResult bindingResult){
+    public BaseResponse<Object> updateFilter(@Parameter(hidden=true) @PreAuthorize long userId, @Valid @RequestBody PatchUpdateFilterRequest patchUpdateFilterRequest){
         log.info("{UserController.updateFilter}");
-
-        if(bindingResult.hasFieldErrors("transactionType")){
-            throw new UserBadRequestException(INVALID_TRANSACTION_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("realEstateType")){
-            throw new UserBadRequestException(INVALID_INTEREST_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("realEstateType")){
-            throw new UserBadRequestException(INVALID_INTEREST_TYPE);
-        }
-        if(bindingResult.hasFieldErrors("priceMin") ||
-                bindingResult.hasFieldErrors("depositMin")){
-            throw  new UserBadRequestException(INVALID_MIN_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("priceMax") ||
-                bindingResult.hasFieldErrors("depositMax")){
-            throw new UserBadRequestException(INVALID_MAX_PRICE);
-        }
-        if(bindingResult.hasFieldErrors("isSmallerThanMax")){
-            throw new UserBadRequestException(MIN_IS_BIGGER_THAN_MAX);
-        }
 
         return new BaseResponse<>(MEMBER_FILTER_UPDATE_SUCCESS, this.userService.updateFilter(userId, patchUpdateFilterRequest));
     }
